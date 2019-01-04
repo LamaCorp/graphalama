1e5a79bd20e5d340371f7e0d13848f3632f2c921
ddorn <diego.dorn@free.fr>
2019-01-04 11:58:47 +0100

    Allow importing CarrouselSwitch from widgets

--- graphalama/widgets.py
+++ graphalama/widgets.py
@@ -1,3 +1,3 @@
-from .buttons import Button, ImageButton, CheckBox
+from .buttons import Button, ImageButton, CheckBox, CarrouselSwitch
 from .text import SimpleText
 from .core import Widget, WidgetList
